1c85c63dd61434ebb91057e9f3fb96ef532f3fbc
rful011 <rful011@9b7c07e5-b204-0410-a359-bf7877acd61e>
2007-07-10 22:36:43 +0000

    Record now inherits from LogFile::Record

--- selms/src/trunk/plugins/Snare.rb
+++ selms/src/trunk/plugins/Snare.rb
@@ -22,29 +22,16 @@ class Snare < LogFile
     @file = File.open( fn )
   end
 
-# default log splitter                                                                                                 
-  class Record
-    attr_reader :time, :utime, :h, :rec, :orec, :data, :criticality, :source, :snareec, :eventid, :source2, :user, :sid, :logtype, :name, :category 
+  class Record < LogFile::Record
+    attr_reader :time, :utime, :h, :rec, :orec, :data, :criticality, :source, :snareec, :eventid, :source2, 
+                :user, :sid, :logtype, :name, :category 
 
-    def initialize( raw, pat, dnmmy = nil )
-      @raw = raw
-      
-      @time = nil
-      @utime = nil
-       @h = nil
-      @orec = nil
-      @data = nil
-      all, @utime, @time, @h,  @data =  raw.match(pat).to_a
-
-    end
-    
-# default log splitter
 
     def split
-#pp data
+
       lt, @criticality, @source, @snareec, dt, @eventid, @source2, @user, @sid,
 	@logtype, @name, @category, xx, s, es  = @data.split("\t")
-#puts data unless s
+
       s = '' unless s
       es = '' unless es
       @data = (s + ' ' + es).sub(/^\s+/, '')
