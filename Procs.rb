4e243c538bf62968c39fe020893a7f89408d3552
rful011 <r.fulton@auckland.ac.nz>
2011-08-19 01:32:38 +0000

    more fixes for pw_check

--- Procs.rb
+++ Procs.rb
@@ -12,7 +12,7 @@ module Procs
       @pw_check.sort{|a,b| b[1].size<=>a[1].size}.each do |entry|
         ip, upis = entry
         puts ip
-        uips.each do |upi, count|
+        upis.each do |upi, count|
           puts "   #{upi}: #{count}"
         end
       end
